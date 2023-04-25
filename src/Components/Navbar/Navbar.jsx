d29f9325ec099916273c1d58e7a1dd99a9b884b0
Natanael Saymon <saymoneo@gmail.com>
2023-04-25 12:00:44 -0300

    feat: navbar estruturado

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,40 @@
+import Link from 'next/link'
 import styles from './Navbar.module.scss'
+import Logo from '@/UI/Logo'
+
+const links = [
+    { url: '#', text: 'Produtos' },
+    { url: '#', text: 'Sobre' },
+    { url: '#', text: 'Social' },
+    { url: '#', text: 'Depoimentos' },
+]
 
 const Navbar = () => {
+    const renderLinks = links.map((link, index) => (
+        <li key={index}>
+            <Link href={link.url}>
+                {link.text}
+            </Link>
+        </li>
+    ))
     return(
-        <div className={styles.navbar}>Navbar</div>
+        <section className={styles.navbar}>
+            <nav className={styles.navigation}>
+                <ul>
+                    {renderLinks.slice(0, 2)}
+                </ul>
+            </nav>
+
+            <div className={styles.logo}>
+                <Logo />
+            </div>
+
+            <nav className={styles.navigation}>
+                <ul>
+                    {renderLinks.slice(2, 4)}
+                </ul>
+            </nav>
+        </section>
     )
 }
 
