113e9e30ac90acc849d2025a33a0fb4188212999
Natanael Saymon <saymoneo@gmail.com>
2023-04-25 12:00:15 -0300

    feat: componente Logo

---
+++ src/UI/Logo.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Logo = () => {
+    return(
+        <Link href="/">
+            <Image src="/logo.svg" alt="bubble coffe" width={77} height={38}/>
+        </Link>
+    )
+}
+
+export default Logo
